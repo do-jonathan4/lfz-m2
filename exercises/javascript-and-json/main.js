476449fc860a7f7469d758e9d0f36075bad407e4
John Do <jdo697@gmail.com>
2020-11-14 11:35:20 -0800

    completed javascript-and-json

--- exercises/javascript-and-json/main.js
+++ exercises/javascript-and-json/main.js
@@ -1,1 +1,30 @@
 /* eslint-disable no-console */
+var book1 = [
+  {
+    isbn: '123456789a',
+    title: 'A',
+    author: 'A'
+  },
+  {
+    isbn: '123456789b',
+    title: 'B',
+    author: 'B'
+  },
+  {
+    isbn: '123456789c',
+    title: 'C',
+    author: 'C'
+  }
+];
+console.log('result: ', JSON.stringify(book1));
+console.log('type: ', typeof JSON.stringify(book1));
+
+var student = JSON.stringify(
+  {
+    id: 1234,
+    name: 'John'
+  }
+);
+
+console.log('result: ', JSON.parse(student));
+console.log('type: ', typeof JSON.parse(student));
