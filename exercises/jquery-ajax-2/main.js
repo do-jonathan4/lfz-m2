ef87effee36260496baa5c41eec57ab0f5b900d2
John Do <jdo697@gmail.com>
2020-11-17 20:48:50 -0800

    completed jquery-ajax-2

--- exercises/jquery-ajax-2/main.js
+++ exercises/jquery-ajax-2/main.js
@@ -1,1 +1,25 @@
-// Your code here :3
+/* global $ */
+$.ajax({
+  type: 'GET',
+  url: 'https://jsonplaceholder.typicode.com/users',
+  success: function (data) {
+
+    for (var i = 0; i < data.length; i++) {
+      var tr = document.createElement('tr');
+      var tdUser = document.createElement('td');
+      var tdName = document.createElement('td');
+      var tdEmail = document.createElement('td');
+      var tbody = document.getElementById('user-data');
+
+      tdUser.textContent = data[i].id;
+      tdName.textContent = data[i].name;
+      tdEmail.textContent = data[i].email;
+
+      tr.appendChild(tdUser);
+      tr.appendChild(tdName);
+      tr.appendChild(tdEmail);
+      tbody.appendChild(tr);
+
+    }
+  }
+});
