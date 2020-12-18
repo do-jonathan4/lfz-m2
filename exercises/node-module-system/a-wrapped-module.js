33124a31379df3ec964153e61447550d7aa257b3
John Do <jdo697@gmail.com>
2020-12-17 21:12:23 -0800

    completed node-module-system

---
+++ exercises/node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('Exports: ' + typeof exports);
+console.log(exports);
+
+console.log('Require: ' + typeof require);
+console.log(require);
+
+console.log('Module: ' + typeof module);
+console.log(module);
+
+console.log('Filename: ' + typeof __filename);
+console.log(__filename);
+
+console.log('Dirname: ' + typeof __dirname);
+console.log(__dirname);
