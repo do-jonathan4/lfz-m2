09a962b7af0a221535c9c8cc0d2fc5d6e2faa583
John Do <jdo697@gmail.com>
2020-12-17 20:36:06 -0800

    completed the-event-loop

--- exercises/the-event-loop/interval.js
+++ exercises/the-event-loop/interval.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-console */
+var countDown = 3;
+
+const blast = setInterval(() => {
+  console.log(countDown);
+  countDown--;
+  if (!countDown) {
+    console.log('Blast off!');
+    clearInterval(blast);
+  }
+}, 1000);
