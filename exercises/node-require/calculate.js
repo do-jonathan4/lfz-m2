9554b7ffab025123c9683308ba34d44a0138934e
John Do <jdo697@gmail.com>
2020-12-19 11:46:00 -0800

    fixed issue

--- exercises/node-require/calculate.js
+++ exercises/node-require/calculate.js
@@ -3,19 +3,19 @@ const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
 
-const num1 = parseInt(process.argv[2]);
+const num1 = parseFloat(process.argv[2]);
 const operator = process.argv[3];
-const num2 = parseInt(process.argv[4]);
+const num2 = parseFloat(process.argv[4]);
 
 if (operator === 'plus') {
-  console.log(`result: ${add(num1, num2)}`);
+  console.log(`result: ${Number(add(num1, num2))}`);
 }
 if (operator === 'minus') {
-  console.log(`result: ${subtract(num1, num2)}`);
+  console.log(`result: ${Number(subtract(num1, num2))}`);
 }
 if (operator === 'times') {
-  console.log(`result: ${multiply(num1, num2)}`);
+  console.log(`result: ${Number(multiply(num1, num2))}`);
 }
 if (operator === 'divide') {
-  console.log(`result: ${divide(num1, num2)}`);
+  console.log(`result: ${Number(divide(num1, num2))}`);
 }
