7af54571d00103a437b4ccd3251ee828aab2e298
John Do <jdo697@gmail.com>
2020-12-31 18:22:16 -0800

    User can delete a note

--- exercises/node-notes-cli/app.js
+++ exercises/node-notes-cli/app.js
@@ -2,10 +2,14 @@ const data = require('./data.json');
 const fileName = './data.json';
 const read = require('./features/read');
 const add = require('./features/add');
+const remove = require('./features/delete');
 
 if (process.argv[2] === 'read') {
   read(data);
 }
 if (process.argv[2] === 'add') {
   add(data, fileName, process.argv[3]);
+}
+if (process.argv[2] === 'delete') {
+  remove(data, fileName, process.argv[3]);
 }
