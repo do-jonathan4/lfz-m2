b66c23392bc68bc6d7e94756e14eca81dc0eeac8
John Do <jdo697@gmail.com>
2021-01-09 12:22:55 -0800

    completed express-post-json

---
+++ exercises/express-post-json/index.js
@@ -0,0 +1,23 @@
+const grades = [];
+let nextId = 1;
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+app.post('/api/grades', (req, res) => {
+  const obj = req.body;
+  obj.id = nextId;
+  grades.push(obj);
+  nextId++;
+  res.status(201).send();
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
