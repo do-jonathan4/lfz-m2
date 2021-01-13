d01b398b8bb3a63b5470f33083475c802ed9ea15
John Do <jdo697@gmail.com>
2021-01-13 02:48:31 -0800

    Clients can GET a single note by id

--- exercises/express-notes-api/index.js
+++ exercises/express-notes-api/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -7,11 +8,26 @@ app.get('/api/grades', (req, res) => {
     if (err) throw err;
     const notesFile = JSON.parse(data);
     const notesArray = Object.values(notesFile.notes);
-    res.json(notesArray);
+    res.status(200).json(notesArray);
+  });
+});
+app.get('/api/grades/:id', (req, res) => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notesID = parseInt(req.params.id);
+    const notesFile = JSON.parse(data);
+    const notesArray = notesFile.notes[notesID];
+
+    if (isNaN(notesID)) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+    } else if (!notesFile.notes.hasOwnProperty(notesID)) {
+      res.status(404).json({ error: `cannot find note with id ${notesID}` });
+    } else {
+      res.status(200).json(notesArray);
+    }
   });
 });
 
 app.listen(3000, () => {
-  // eslint-disable-next-line no-console
   console.log('Listening on port 3000!');
 });
