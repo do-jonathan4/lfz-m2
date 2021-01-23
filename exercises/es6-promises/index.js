7575fbc5c78f9be7f88cf64f120e65f83196255f
John Do <jdo697@gmail.com>
2021-01-23 10:30:39 -0800

    completed es6-promises

--- exercises/es6-promises/index.js
+++ exercises/es6-promises/index.js
@@ -1,1 +1,11 @@
 const takeAChance = require('./take-a-chance');
+
+var myName = takeAChance('John');
+
+myName.then(value => {
+  console.log(value);
+});
+
+myName.catch(error => {
+  console.error(error);
+});
