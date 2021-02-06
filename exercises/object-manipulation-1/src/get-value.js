c64e87192beff89e0a77587c6298b7db3a1d1344
John Do <jdo697@gmail.com>
2021-02-06 10:59:33 -0800

    completed object-manipulation-1

--- exercises/object-manipulation-1/src/get-value.js
+++ exercises/object-manipulation-1/src/get-value.js
@@ -1,1 +1,4 @@
 /* eslint-disable no-unused-vars */
+function getValues(object) {
+  return Object.values(object);
+}
--- exercises/object-manipulation-1/src/to-object.js
+++ exercises/object-manipulation-1/src/to-object.js
@@ -1,1 +1,6 @@
 /* eslint-disable no-unused-vars */
+function toObject(keyValuePair) {
+  var newObj = {};
+  newObj[keyValuePair[0]] = keyValuePair[1];
+  return newObj;
+}
