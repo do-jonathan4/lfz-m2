44996894b43ed0abb5474476e189bea8d9687f9a
John Do <jdo697@gmail.com>
2021-02-06 11:20:44 -0800

    temp

--- exercises/string-manipulation-2/src/truncate.js
+++ exercises/string-manipulation-2/src/truncate.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+function truncate(length, string) {
+  var newString = '';
+  if (length < string.length) {
+    for (let i = 0; i < length; i++) {
+      newString += string[i];
+    }
+  }
+  if (length > string.length) {
+    for (let i = 0; i < string.length; i++) {
+      newString += string[i];
+    }
+  }
+
+  newString += '...';
+  return newString;
+}
