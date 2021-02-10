8d3118d6305a46537f514444e74f8e0d13d5b196
John Do <jdo697@gmail.com>
2021-02-10 14:16:46 -0800

    completed object-manipulation-2

--- exercises/object-manipulation-2/src/defaults.js
+++ exercises/object-manipulation-2/src/defaults.js
@@ -1,1 +1,18 @@
 /* exported defaults */
+function defaults(target, source) {
+  const targetKeys = [];
+  const srcKeys = [];
+
+  for (const prop in target) {
+    targetKeys.push(prop);
+  }
+  for (const prop in source) {
+    srcKeys.push(prop);
+  }
+
+  const defaultKeys = srcKeys.filter(x => targetKeys.indexOf(x) === -1);
+
+  for (let i = 0; i < defaultKeys.length; i++) {
+    target[defaultKeys[i]] = source[defaultKeys[i]];
+  }
+}
--- exercises/object-manipulation-2/src/omit.js
+++ exercises/object-manipulation-2/src/omit.js
@@ -1,1 +1,17 @@
 /* exported omit */
+function omit(source, keys) {
+  const newObj = {};
+  const srcKeys = [];
+
+  for (const prop in source) {
+    srcKeys.push(prop);
+  }
+
+  const newKeys = srcKeys.filter(x => keys.indexOf(x) === -1);
+
+  for (let i = 0; i < newKeys.length; i++) {
+    newObj[newKeys[i]] = source[newKeys[i]];
+  }
+
+  return newObj;
+}
